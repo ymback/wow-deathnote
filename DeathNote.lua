c2eb77e1c78dbe704e89de3fdc2b034fb79ff1a9
Anh Lai <ymback@sayyoulove.me>
2019-09-12 11:05:41 +0800

    Base File
    Signed-off-by: Anh Lai <ymback@sayyoulove.me>

---
+++ DeathNote.lua
@@ -0,0 +1,14 @@
+## Interface: 11302
+## Title: DeathNote v0.1
+## Version: 0.1
+## Author: ymback/grpubr/bigqiao
+
+## SavedVariables: DeathNote_Record,DeathNote_Profile
+
+## Notes: Keep fucking character away from you!
+
+Common.lua
+Core.lua
+Keep.lua
+Async.lua
+SendMessage.lua
